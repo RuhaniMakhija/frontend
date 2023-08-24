52331c34ce2d8b615c09403d65ae49dca09afeca
Ruhani FashionFantasy <ruhanifashionfantasy@Ruhanis-MacBook-Air.local>
2023-08-24 20:57:37 +0530

    frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Register from './components/Register/Register'
+import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
+import Login from './components/Login/Login';
+import LoginRegister from './components/LoginRegister/LoginRegister';
+import Welcome from './components/Welcome/Welcome';
+import Developer from './components/Developer/Developer';
+import Selfhosting from './components/Selfhosting/Selfhosting';
+import SelfRepo from './components/SelfRepo/SelfRepo';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Routes>
+      
+        <Route path='/register' element={<Register/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/welcome' element={<Welcome/>}/>
+        <Route path='/developer' element={<Developer/>}/>
+        <Route path='/self' element={<Selfhosting/>}/>
+        <Route path='/self-repo' element={<SelfRepo/>}/>
+
+
+
+
+   
+      </Routes>
+    
+  )
 }
 
-export default App;
+export default App
+  
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { GoogleOAuthProvider,GoogleLogin } from '@react-oauth/google';
+import jwt_decode from 'jwt-decode';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Router>
+    <GoogleOAuthProvider clientId="313867018675-7m2p4hoiad717l2qpqao2lmplsqhr103.apps.googleusercontent.com">
+    
+
+
     <App />
-  </React.StrictMode>
+    </GoogleOAuthProvider>
+    </Router>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
